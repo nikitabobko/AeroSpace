d264a9162f4e111f7da15b8b53bd76498896ee46
Nikita Bobko <git@bobko.xyz>
2023-05-21 21:06:35 +0200

    Remove storyboard target

--- storyboard/main.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import AppKit
-
-let app = NSApplication.shared
-let delegate = AppDelegate()
-app.delegate = delegate
-
-_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
