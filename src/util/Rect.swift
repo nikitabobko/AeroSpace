cf11b16a5315d49085f24bbfa2d5c1a98c9c1bcb
Nikita Bobko <git@bobko.xyz>
2023-08-26 23:03:33 +0200

    Clenup: extract Rect to a separate file

---
+++ src/util/Rect.swift
@@ -0,0 +1,63 @@
+import Foundation
+
+struct Rect: Hashable {
+    let topLeftX: CGFloat
+    let topLeftY: CGFloat
+    let width: CGFloat
+    let height: CGFloat
+}
+
+extension [Rect] {
+    func union() -> Rect {
+        let rects: [Rect] = self
+        let topLeftY = rects.map { $0.minY }.minOrThrow()
+        let topLeftX = rects.map { $0.minX }.maxOrThrow()
+        return Rect(
+                topLeftX: topLeftX,
+                topLeftY: topLeftY,
+                width: rects.map { $0.maxX }.maxOrThrow() - topLeftX,
+                height: rects.map { $0.maxY}.maxOrThrow() - topLeftY
+        )
+    }
+}
+
+extension CGRect {
+    func monitorFrameNormalized() -> Rect {
+        let mainMonitorHeight: CGFloat = NSScreen.screens.firstOrThrow { $0.isMainMonitor }.frame.height
+        let rect = toRect()
+        return rect.copy(topLeftY: mainMonitorHeight - rect.topLeftY)
+    }
+}
+
+extension CGRect {
+    func toRect() -> Rect {
+        Rect(topLeftX: minX, topLeftY: maxY, width: width, height: height)
+    }
+}
+
+extension Rect {
+    func contains(_ point: CGPoint) -> Bool {
+        let x = point.x
+        let y = point.y
+        return (minX..<maxX).contains(x) && (minY..<maxY).contains(y)
+    }
+
+    func copy(topLeftX: CGFloat? = nil, topLeftY: CGFloat? = nil, width: CGFloat? = nil, height: CGFloat? = nil) -> Rect {
+        Rect(
+                topLeftX: topLeftX ?? self.topLeftX,
+                topLeftY: topLeftY ?? self.topLeftY,
+                width: width ?? self.width,
+                height: height ?? self.height
+        )
+    }
+
+    var topLeftCorner: CGPoint { CGPoint(x: topLeftX, y: topLeftY) }
+    var topRightCorner: CGPoint { CGPoint(x: maxX, y: minY) }
+    var bottomRightCorner: CGPoint { CGPoint(x: maxX, y: maxY) }
+    var bottomLeftCorner: CGPoint { CGPoint(x: minX, y: maxY) }
+
+    var minY: CGFloat { topLeftY }
+    var maxY: CGFloat { topLeftY + height }
+    var minX: CGFloat { topLeftX }
+    var maxX: CGFloat { topLeftX + width }
+}
--- src/util/utils.swift
+++ src/util/utils.swift
@@ -65,41 +65,6 @@ extension NSScreen {
     var visibleRect: Rect { visibleFrame.monitorFrameNormalized() }
 }
 
-extension CGRect {
-    func monitorFrameNormalized() -> Rect {
-        let mainMonitorHeight: CGFloat = NSScreen.screens.firstOrThrow { $0.isMainMonitor }.frame.height
-        let rect = toRect()
-        return rect.copy(topLeftY: mainMonitorHeight - rect.topLeftY)
-    }
-}
-
-extension CGRect {
-    func toRect() -> Rect {
-        Rect(topLeftX: minX, topLeftY: maxY, width: width, height: height)
-    }
-}
-
-struct Rect: Hashable {
-    let topLeftX: CGFloat
-    let topLeftY: CGFloat
-    let width: CGFloat
-    let height: CGFloat
-}
-
-extension [Rect] {
-    func union() -> Rect {
-        let rects: [Rect] = self
-        let topLeftY = rects.map { $0.minY }.minOrThrow()
-        let topLeftX = rects.map { $0.minX }.maxOrThrow()
-        return Rect(
-                topLeftX: topLeftX,
-                topLeftY: topLeftY,
-                width: rects.map { $0.maxX }.maxOrThrow() - topLeftX,
-                height: rects.map { $0.maxY}.maxOrThrow() - topLeftY
-        )
-    }
-}
-
 extension Double {
     var squared: Double { self * self }
 }
@@ -147,33 +112,6 @@ extension CGSize {
     func copy(width: Double? = nil, height: Double? = nil) -> CGSize {
         CGSize(width: width ?? self.width, height: height ?? self.height)
     }
-}
-
-extension Rect {
-    func contains(_ point: CGPoint) -> Bool {
-        let x = point.x
-        let y = point.y
-        return (minX..<maxX).contains(x) && (minY..<maxY).contains(y)
-    }
-
-    func copy(topLeftX: CGFloat? = nil, topLeftY: CGFloat? = nil, width: CGFloat? = nil, height: CGFloat? = nil) -> Rect {
-        Rect(
-                topLeftX: topLeftX ?? self.topLeftX,
-                topLeftY: topLeftY ?? self.topLeftY,
-                width: width ?? self.width,
-                height: height ?? self.height
-        )
-    }
-
-    var topLeftCorner: CGPoint { CGPoint(x: topLeftX, y: topLeftY) }
-    var topRightCorner: CGPoint { CGPoint(x: maxX, y: minY) }
-    var bottomRightCorner: CGPoint { CGPoint(x: maxX, y: maxY) }
-    var bottomLeftCorner: CGPoint { CGPoint(x: minX, y: maxY) }
-
-    var minY: CGFloat { topLeftY }
-    var maxY: CGFloat { topLeftY + height }
-    var minX: CGFloat { topLeftX }
-    var maxX: CGFloat { topLeftX + width }
 }
 
 extension Sequence {
