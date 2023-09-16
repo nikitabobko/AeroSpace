69f0d12c1be272cb9d527b43aac1a5454f8594df
Nikita Bobko <git@bobko.xyz>
2023-09-16 16:49:43 +0200

    Run xcodegen in run-* scripts

--- run-tests.sh
+++ run-tests.sh
@@ -5,4 +5,5 @@ set -o pipefail # Any command failed in the pipe fails the whole pipe
 # set -x # Print shell commands as they are executed (or you can try -v which is less verbose)
 
 cd "$(dirname "$0")"
+xcodegen # https://github.com/yonaskolb/XcodeGen
 xcodebuild -scheme AeroSpace test
