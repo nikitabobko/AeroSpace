646fa5b30b636d83d52c781a6a1cb3a56408d178
Nikita Bobko <git@bobko.xyz>
2023-10-11 01:03:12 +0200

    WorkspaceCommand: make sure that stack of windows is correct from macOS perspective

--- src/command/WorkspaceCommand.swift
+++ src/command/WorkspaceCommand.swift
@@ -6,6 +6,9 @@ struct WorkspaceCommand : Command {
         let workspace = Workspace.get(byName: workspaceName)
         debug("Switch to workspace: \(workspace.name)")
         if let window = workspace.mostRecentWindow ?? workspace.anyLeafWindowRecursive { // switch to not empty workspace
+            workspace.focusMruReversedRecursive()
+            // Technically, it must not be necessary. But this way, it's more chances
+            // that the correct window will end up focused at the end
             window.focus()
             // The switching itself will be done by refreshWorkspaces and layoutWorkspaces later in refresh
         } else { // switch to empty workspace
@@ -22,5 +25,17 @@ struct WorkspaceCommand : Command {
 
     static func switchToWorkspace(_ workspace: Workspace) async {
         await WorkspaceCommand(workspaceName: workspace.name).run()
+    }
+}
+
+private extension TreeNode {
+    // todo Switch between workspaces can happen via cmd+tab. Maybe this functionality must be moved to refresh
+    func focusMruReversedRecursive() {
+        if let window = self as? Window {
+            window.focus()
+        }
+        for child in mostRecentChildren.reversed() {
+            child.focusMruReversedRecursive()
+        }
     }
 }
