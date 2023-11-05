5eb56be318d5662ff872d898b1783dec7133014b
Nikita Bobko <git@bobko.xyz>
2023-11-05 20:10:22 +0100

    MoveNodeToWorkspaceCommand: focusedWindowOrEffectivelyFocused

--- src/command/MoveNodeToWorkspaceCommand.swift
+++ src/command/MoveNodeToWorkspaceCommand.swift
@@ -2,7 +2,7 @@ struct MoveNodeToWorkspaceCommand: Command {
     let targetWorkspaceName: String
 
     func runWithoutLayout() async {
-        guard let focused = focusedWindow else { return }
+        guard let focused = focusedWindowOrEffectivelyFocused else { return }
         let preserveWorkspace = focused.workspace
         let targetWorkspace = Workspace.get(byName: targetWorkspaceName)
         let targetContainer: NonLeafTreeNode = focused.isFloating ? targetWorkspace : targetWorkspace.rootTilingContainer
