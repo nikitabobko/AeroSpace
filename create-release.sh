1443b345b9cc0425f3b32f24fad926266155586f
Nikita Bobko <git@bobko.xyz>
2023-11-11 02:44:29 +0100

    new file: create-release.sh

---
+++ create-release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e # Exit if one of commands exit with non-zero exit code
+set -u # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
+set -o pipefail # Any command failed in the pipe fails the whole pipe
+# set -x # Print shell commands as they are executed (or you can try -v which is less verbose)
+
+cd "$(dirname "$0")"
+git tag -a v$1 -m "v$1" && git push --tags
+open "https://github.com/nikitabobko/AeroSpace/releases/new?tag=v$1"
