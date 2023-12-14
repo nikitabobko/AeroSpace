e053a6c28bba3b0d22b14442de3a19d184e2f14d
Nikita Bobko <git@bobko.xyz>
2023-12-14 21:50:14 +0100

    docs

---
+++ copy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e # Exit if one of commands exit with non-zero exit code
+set -u # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
+set -o pipefail # Any command failed in the pipe fails the whole pipe
+# set -x # Print shell commands as they are executed (or you can try -v which is less verbose)
+
+cd "$(dirname "$0")"
+
+rm -rf config-examples
+rm -rf docs
+
+cp -r $1/config-examples .
+cp -r $1/.docs docs
