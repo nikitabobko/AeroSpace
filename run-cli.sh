38c6e19b54a6d1d04167102aa478e14cde0b99e6
Nikita Bobko <git@bobko.xyz>
2023-12-28 17:06:25 +0100

    new file: run-cli.sh

---
+++ run-cli.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# set -e # Exit if one of commands exit with non-zero exit code
+set -u # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
+set -o pipefail # Any command failed in the pipe fails the whole pipe
+# set -x # Print shell commands as they are executed (or you can try -v which is less verbose)
+
+# I'd not need this script if `swift run` wasn't that damn slow compared to `swift build`!
+# https://forums.swift.org/t/swift-run-really-slow/67807/12
+cd "$(dirname "$0")"
+
+cd ./LocalPackage
+    output="$(swift build)"
+    if [ $? -ne 0 ]; then
+        echo "$output"
+        exit 1
+    fi
+cd - > /dev/null
+
+./LocalPackage/.build/debug/aerospace "$@"
