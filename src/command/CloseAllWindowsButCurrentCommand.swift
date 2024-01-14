8b8ebf51f37f8cb7ec12b453eebed7323dd5ffb9
Nikita Bobko <git@bobko.xyz>
2024-01-14 20:11:54 +0100

    Fix CloseAllWindowsButCurrentCommand

--- src/command/CloseAllWindowsButCurrentCommand.swift
+++ src/command/CloseAllWindowsButCurrentCommand.swift
@@ -12,9 +12,11 @@ struct CloseAllWindowsButCurrentCommand: Command {
         var result = true
         for window in focused.workspace.allLeafWindowsRecursive {
             if window != focused {
+                state.subject = .window(window)
                 result = CloseCommand().run(state) && result
             }
         }
+        state.subject = .window(focused)
         return result
     }
 }
