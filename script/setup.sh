e77800f39ec5523172fb5724066b238192abd440
Nikita Bobko <git@bobko.xyz>
2024-02-04 01:25:16 +0100

    Make sure that set flags are set for every bash script
    Motivation: failing run-test.sh in script/publish-release.sh isn't
    reported

--- script/setup.sh
+++ script/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
+set -e # Exit if one of commands exit with non-zero exit code
+set -u # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
+set -o pipefail # Any command failed in the pipe fails the whole pipe
+# set -x # Print shell commands as they are executed (or you can try -v which is less verbose)
 
 setup() {
-    set -e # Exit if one of commands exit with non-zero exit code
-    set -u # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
-    set -o pipefail # Any command failed in the pipe fails the whole pipe
-    # set -x # Print shell commands as they are executed (or you can try -v which is less verbose)
     tmp=(
         $(brew --prefix)/opt/asciidoctor/bin
         $(brew --prefix)/opt/gsed/libexec/gnubin
@@ -19,7 +19,7 @@ setup() {
     unset IFS
 }
 
-if [ -z "$SETUP_SH" ]; then
+if [ -z "${SETUP_SH:-}" ]; then
     export SETUP_SH=true
     setup
 fi
