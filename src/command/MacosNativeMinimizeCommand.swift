e417aad710235d1e6d94b1000e48bb69cdf54f0f
Nikita Bobko <git@bobko.xyz>
2024-03-09 00:43:53 +0100

    MacosNativeMinimizeCommand: don't forget to update the subject at the end of the command

--- src/command/MacosNativeMinimizeCommand.swift
+++ src/command/MacosNativeMinimizeCommand.swift
@@ -11,8 +11,29 @@ struct MacosNativeMinimizeCommand: Command {
             return false
         }
         let axWindow = window.asMacWindow().axWindow
-        let success = axWindow.set(Ax.minimizedAttr, !window.isMacosMinimized)
-        if !success { state.stderr.append("Failed") }
-        return success
+        let newState: Bool = !window.isMacosMinimized
+        if axWindow.set(Ax.minimizedAttr, newState) {
+            let workspace = window.unbindFromParent().parent.workspace ?? Workspace.focused
+            if newState {
+                window.bind(to: macosInvisibleWindowsContainer, adaptiveWeight: 1, index: INDEX_BIND_LAST)
+                if let mru = workspace.mostRecentWindow {
+                    state.subject = .window(mru)
+                } else {
+                    state.subject = .emptyWorkspace(workspace.name)
+                }
+            } else {
+                switch window.layoutReason {
+                case .macos(let prevParentKind):
+                    macOsExitMacOsNativeOrInvisibleState(window: window, prevParentKind: prevParentKind, workspace: workspace)
+                default: // wtf case. Theoretically should never happen
+                    window.relayoutWindow(on: workspace)
+                }
+                state.subject = .window(window)
+            }
+            return true
+        } else {
+            state.stderr.append("Failed")
+            return false
+        }
     }
 }
--- src/normalizeLayoutReason.swift
+++ src/normalizeLayoutReason.swift
@@ -6,7 +6,7 @@ func normalizeLayoutReason() {
     _normalizeLayoutReason(workspace: Workspace.focused, windows: macosInvisibleWindowsContainer.children.filterIsInstance(of: Window.self))
 }
 
-func _normalizeLayoutReason(workspace: Workspace, windows: [Window]) {
+private func _normalizeLayoutReason(workspace: Workspace, windows: [Window]) {
     for window in windows {
         let isMacosFullscreen = window.isMacosFullscreen
         let isMacosInvisible = !isMacosFullscreen && (window.isMacosMinimized || window.macAppUnsafe.nsApp.isHidden)
@@ -23,19 +23,29 @@ func _normalizeLayoutReason(workspace: Workspace, windows: [Window]) {
             }
         case .macos(let prevParentKind):
             if !isMacosFullscreen && !isMacosInvisible {
-                window.layoutReason = .standard
-                window.unbindFromParent()
-                switch prevParentKind {
-                case .workspace:
-                    window.bindAsFloatingWindow(to: workspace)
-                case .tilingContainer:
-                    let data = getBindingDataForNewTilingWindow(workspace)
-                    window.bind(to: data.parent, adaptiveWeight: data.adaptiveWeight, index: data.index)
-                case .macosInvisibleWindowsContainer, .macosFullscreenWindowsContainer: // wtf case, should never be possible. But If encounter it, let's just re-layout window
-                    let data = getBindingDataForNewWindow(window.asMacWindow().axWindow, workspace, window.macAppUnsafe)
-                    window.bind(to: data.parent, adaptiveWeight: data.adaptiveWeight, index: data.index)
-                }
+                macOsExitMacOsNativeOrInvisibleState(window: window, prevParentKind: prevParentKind, workspace: workspace)
             }
         }
+    }
+}
+
+func macOsExitMacOsNativeOrInvisibleState(window: Window, prevParentKind: NonLeafTreeNodeKind, workspace: Workspace) {
+    window.layoutReason = .standard
+    window.unbindFromParent()
+    switch prevParentKind {
+    case .workspace:
+        window.bindAsFloatingWindow(to: workspace)
+    case .tilingContainer:
+        let data = getBindingDataForNewTilingWindow(workspace)
+        window.bind(to: data.parent, adaptiveWeight: data.adaptiveWeight, index: data.index)
+    case .macosInvisibleWindowsContainer, .macosFullscreenWindowsContainer: // wtf case, should never be possible. But If encounter it, let's just re-layout window
+        window.relayoutWindow(on: workspace)
+    }
+}
+
+extension Window {
+    func relayoutWindow(on workspace: Workspace) {
+        let data = getBindingDataForNewWindow(self.asMacWindow().axWindow, workspace, self.macAppUnsafe)
+        bind(to: data.parent, adaptiveWeight: data.adaptiveWeight, index: data.index)
     }
 }
