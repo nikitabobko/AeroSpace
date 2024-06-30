9294d56748c16add0d4669e1b30e7ad0281ebcf7
Nikita Bobko <git@bobko.xyz>
2024-06-30 20:20:26 +0200

    4/n list-monitors: support --format flag

--- Sources/AppBundle/command/query/ListMonitorsCommand.swift
+++ Sources/AppBundle/command/query/ListMonitorsCommand.swift
@@ -6,19 +6,20 @@ struct ListMonitorsCommand: Command {
 
     func _run(_ state: CommandMutableState, stdin: String) -> Bool {
         check(Thread.current.isMainThread)
-        var result: [(Int, Monitor)] = sortedMonitors.withIndex
+        var result = sortedMonitors
         if let focused = args.focused {
-            result = result.filter { (_, monitor) in (monitor.activeWorkspace == Workspace.focused) == focused }
+            result = result.filter { (monitor) in (monitor.activeWorkspace == Workspace.focused) == focused }
         }
         if let mouse = args.mouse {
             let mouseWorkspace = mouseLocation.monitorApproximation.activeWorkspace
-            result = result.filter { (_, monitor) in (monitor.activeWorkspace == mouseWorkspace) == mouse }
+            result = result.filter { (monitor) in (monitor.activeWorkspace == mouseWorkspace) == mouse }
         }
-        state.stdout += result
-            .map { (index, monitor) in
-                [String(index + 1), monitor.name]
-            }
-            .toPaddingTable()
-        return true
+        switch result.map({ AeroObj.monitor($0) }).format(args.format) {
+            case .success(let lines):
+                state.stdout += lines
+                return true
+            case .failure(let msg):
+                return state.failCmd(msg: msg)
+        }
     }
 }
--- Sources/Common/cmdArgs/query/ListMonitorsCmdArgs.swift
+++ Sources/Common/cmdArgs/query/ListMonitorsCmdArgs.swift
@@ -3,22 +3,28 @@ public struct ListMonitorsCmdArgs: RawCmdArgs, CmdArgs, Equatable {
         kind: .listMonitors,
         allowInConfig: false,
         help: """
-            USAGE: list-monitors [-h|--help] [--focused [no]] [--mouse [no]]
+            USAGE: list-monitors [-h|--help] [--focused [no]] [--mouse [no]] [--format <output-format>]
 
             OPTIONS:
-              -h, --help       Print help
-              --focused [no]   Filter results to only print the focused monitor
-              --mouse [no]     Filter results to only print the monitor with the mouse
+              -h, --help                 Print help
+              --focused [no]             Filter results to only print the focused monitor
+              --mouse [no]               Filter results to only print the monitor with the mouse
+              --format <output-format>   Specify output format
             """,
         options: [
             "--focused": boolFlag(\.focused),
-            "--mouse": boolFlag(\.mouse)
+            "--mouse": boolFlag(\.mouse),
+            "--format": ArgParser(\.format, parseFormat),
         ],
         arguments: []
     )
 
     public var focused: Bool?
     public var mouse: Bool?
+    public var format: [StringInterToken] = [
+        .value("monitor-id"), .value("right-padding"), .literal(" | "),
+        .value("monitor-name"),
+    ]
 
     public let rawArgs: EquatableNoop<[String]>
     public init(rawArgs: [String]) { self.rawArgs = .init(rawArgs) }
