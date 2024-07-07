4c880e1a98dba93af55df165008ed32a3fa26eff
Nikita Bobko <git@bobko.xyz>
2024-07-07 23:49:52 +0200

    Move files

--- generate-aero-shell-parser.sh
+++ generate-aero-shell-parser.sh
@@ -4,8 +4,12 @@ source ./script/setup.sh
 
 test -e ./.deps/python-venv/bin/antlr4 || ./script/install-antlr.sh
 source ./.deps/python-venv/bin/activate
-antlr4 -no-listener -Dlanguage=Swift -o ./Sources/AeroShellParserGenerated ./grammar/AeroShellLexer.g4 ./grammar/AeroShellParser.g4
+antlr4 -no-listener -Dlanguage=Swift \
+    -o ./Sources/AeroShellParserGenerated \
+    ./grammar/AeroShellLexer.g4\
+    ./grammar/AeroShellParser.g4
 
 # Antlr generates weird *.interp and *.tokens files
 rm ./Sources/AeroShellParserGenerated/grammar/*.interp
 rm ./Sources/AeroShellParserGenerated/grammar/*.tokens
+mv ./Sources/AeroShellParserGenerated/grammar/* ./Sources/AeroShellParserGenerated/
