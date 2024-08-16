b2229939c1a61259314533211c48032151d4cf6b
jakehl <jake@twilsoft.uk>
2024-08-16 21:24:18 +0100

    updates scripts to use SPM in debug builds
    https://github.com/nikitabobko/AeroSpace/pull/424
    Co-authored-by: git@bobko.xyz

--- build-app-bundle-debug.sh
+++ build-app-bundle-debug.sh
@@ -4,7 +4,8 @@ source ./script/setup.sh
 
 ./generate.sh "$@"
 swift build
+xcodebuild -scheme AeroSpace build -configuration Debug -derivedDataPath .xcode-build
 
 rm -rf .debug && mkdir .debug
+cp -r .xcode-build/Build/Products/Debug/AeroSpace-Debug.app .debug
 cp -r .build/debug/aerospace .debug
-cp -r .build/debug/AeroSpaceApp .debug
--- run-debug.sh
+++ run-debug.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 source ./script/setup.sh
 
 ./build-debug.sh
-./.debug/AeroSpace-Debug.app/Contents/MacOS/AeroSpace-Debug
+./.debug/AeroSpaceApp
