561d981b65343ff46e9db4b5354d6cc4d3bde383
jakehl <jake@twilsoft.uk>
2024-08-16 21:23:34 +0100

    updates project structure to share entire codebase between SPM & xcode

---
+++ Sources/PrivateApi/include/private.h
@@ -0,0 +1,8 @@
+#ifndef private_header_h
+#define private_header_h
+
+#import <ApplicationServices/ApplicationServices.h>
+
+AXError _AXUIElementGetWindow(AXUIElementRef element, uint32_t *identifier);
+
+#endif
