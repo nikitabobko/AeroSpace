c6198962b757d9caba6f873c76a1fd90ab0c21fc
jakehl <jake@twilsoft.uk>
2024-08-16 21:42:47 +0100

    updates run scripts to use SPM builds

--- run-cli.sh
+++ run-cli.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")"
-source ./script/setup.sh
 
 # I'd not need this script if `swift run` wasn't that damn slow compared to `swift build`!
 # https://forums.swift.org/t/swift-run-really-slow/67807/12
--- run-debug.sh
+++ run-debug.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")"
-source ./script/setup.sh
-
 ./build-debug.sh
-./.debug/AeroSpace-Debug.app/Contents/MacOS/AeroSpace-Debug
+./.debug/AeroSpaceApp
