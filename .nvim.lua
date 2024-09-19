75bd6cffdb9a36fada918b6a224a4c22bd388af4
Nikita Bobko <git@bobko.xyz>
2024-09-19 23:11:11 +0200

    Configure make command in nvim to compile the project
    Also make build-debug.sh to build test target as well

---
+++ .nvim.lua
@@ -0,0 +1,4 @@
+-- See https://neovim.io/doc/user/options.html#'exrc'
+-- tldr: vim.opt.exrc = true
+
+vim.opt.makeprg = './build-debug.sh'
--- build-debug.sh
+++ build-debug.sh
@@ -4,6 +4,7 @@ source ./script/setup.sh
 
 ./generate.sh --ignore-xcodeproj
 swift build
+swift build --target AppBundleTests # swift build doesn't build test targets by default :(
 
 rm -rf .debug && mkdir .debug
 cp -r .build/debug/aerospace .debug
