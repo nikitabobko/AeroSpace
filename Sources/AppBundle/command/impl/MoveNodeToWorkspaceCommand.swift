fea2349a0afed8e33c4acf4c56a35f39b75d267d
Nikita Bobko <git@bobko.xyz>
2024-09-22 00:58:10 +0200

    move-node-to-workspace. Fix bug. Keep focused workspace focused even when --window-id is specified

--- Sources/AppBundle/command/impl/MoveNodeToWorkspaceCommand.swift
+++ Sources/AppBundle/command/impl/MoveNodeToWorkspaceCommand.swift
@@ -4,26 +4,31 @@ struct MoveNodeToWorkspaceCommand: Command {
     let args: MoveNodeToWorkspaceCmdArgs
 
     func run(_ env: CmdEnv, _ io: CmdIo) -> Bool {
-        guard let focus = args.resolveFocusOrReportError(env, io) else { return false }
-        guard let window = focus.windowOrNil else {
+        let ws = focus.workspace
+        defer { check(ws.focusWorkspace()) } // When moving nodes, the focused workspace must stay unchanged
+
+        guard let subject = args.resolveFocusOrReportError(env, io) else { return false }
+        guard let window = subject.windowOrNil else {
             return io.err(noWindowIsFocused)
         }
-        let prevWorkspace = window.workspace ?? focus.workspace
+        let subjectWs = window.workspace
         let targetWorkspace: Workspace
         switch args.target.val {
             case .relative(let isNext):
-                guard let workspace = getNextPrevWorkspace(current: prevWorkspace, isNext: isNext, wrapAround: args.wrapAround, stdin: io.readStdin()) else { return false }
-                targetWorkspace = workspace
+                guard let subjectWs else { return io.err("Window \(window.windowId) doesn't belong to any workspace") }
+                let ws = getNextPrevWorkspace(current: subjectWs, isNext: isNext, wrapAround: args.wrapAround, stdin: io.readStdin())
+                guard let ws else { return false }
+                targetWorkspace = ws
             case .direct(let name):
                 targetWorkspace = Workspace.get(byName: name.raw)
         }
-        if prevWorkspace == targetWorkspace {
+        if subjectWs == targetWorkspace {
             io.err("Window '\(window.windowId)' already belongs to workspace '\(targetWorkspace.name)'. Tip: use --fail-if-noop to exit with non-zero code")
             return !args.failIfNoop
         }
         let targetContainer: NonLeafTreeNodeObject = window.isFloating ? targetWorkspace : targetWorkspace.rootTilingContainer
         window.bind(to: targetContainer, adaptiveWeight: WEIGHT_AUTO, index: INDEX_BIND_LAST)
-        return prevWorkspace.focusWorkspace()
+        return true
     }
 
     public static func run(_ env: CmdEnv, _ io: CmdIo, _ name: String) -> Bool {
--- Sources/AppBundleTests/command/MoveNodeToWorkspaceCommandTest.swift
+++ Sources/AppBundleTests/command/MoveNodeToWorkspaceCommandTest.swift
@@ -27,20 +27,18 @@ final class MoveNodeToWorkspaceCommandTest: XCTestCase {
             _ = TestWindow.new(id: 1, parent: $0).focusWindow()
         }
 
-        _ = MoveNodeToWorkspaceCommand(args: MoveNodeToWorkspaceCmdArgs(workspace: "b")).run(.defaultEnv, .emptyStdin)
+        MoveNodeToWorkspaceCommand(args: MoveNodeToWorkspaceCmdArgs(workspace: "b")).run(.defaultEnv, .emptyStdin)
         assertEquals(focus.workspace.name, "a")
     }
 
     func testAnotherWindowSubject() {
-        let workspaceA = Workspace.get(byName: "a")
-        var window1: Window!
-        workspaceA.rootTilingContainer.apply {
-            window1 = TestWindow.new(id: 1, parent: $0)
+        Workspace.get(byName: "a").rootTilingContainer.apply {
+            TestWindow.new(id: 1, parent: $0)
             _ = TestWindow.new(id: 2, parent: $0).focusWindow()
         }
 
-        _ = MoveNodeToWorkspaceCommand(args: MoveNodeToWorkspaceCmdArgs(workspace: "b")).run(.defaultEnv, .emptyStdin)
-        assertEquals(focus.windowOrNil, window1)
+        MoveNodeToWorkspaceCommand(args: MoveNodeToWorkspaceCmdArgs(workspace: "b")).run(.defaultEnv, .emptyStdin)
+        assertEquals(focus.windowOrNil?.windowId, 1)
     }
 
     func testPreserveFloatingLayout() {
@@ -51,5 +49,25 @@ final class MoveNodeToWorkspaceCommandTest: XCTestCase {
         MoveNodeToWorkspaceCommand(args: MoveNodeToWorkspaceCmdArgs(workspace: "b")).run(.defaultEnv, .emptyStdin)
         XCTAssertTrue(workspaceA.isEffectivelyEmpty)
         assertEquals(Workspace.get(byName: "b").children.filterIsInstance(of: Window.self).singleOrNil()?.windowId, 1)
+    }
+
+    func testSummonWindow() {
+        let workspaceA = Workspace.get(byName: "a").apply {
+            $0.rootTilingContainer.apply {
+                _ = TestWindow.new(id: 1, parent: $0).focusWindow()
+            }
+        }
+        Workspace.get(byName: "b").rootTilingContainer.apply {
+            TestWindow.new(id: 2, parent: $0)
+        }
+
+        assertEquals(focus.workspace, workspaceA)
+
+        MoveNodeToWorkspaceCommand(args: MoveNodeToWorkspaceCmdArgs(workspace: "a").copy(\.windowId, 2))
+            .run(.defaultEnv, .emptyStdin)
+
+        assertEquals(focus.workspace, workspaceA)
+        assertEquals(Workspace.get(byName: "b").rootTilingContainer.children.count, 0)
+        assertEquals(workspaceA.rootTilingContainer.children.count, 2)
     }
 }
