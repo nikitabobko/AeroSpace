04c3c23ecfe60a11e46d6b17e9919d71378d1bc0
of raisjn <raisjn@okay>
2024-10-20 11:10:12 -0700

    workspaces can swap across monitors when allowed

--- Sources/AppBundle/command/impl/SummonWorkspaceCommand.swift
+++ Sources/AppBundle/command/impl/SummonWorkspaceCommand.swift
@@ -7,16 +7,27 @@ struct SummonWorkspaceCommand: Command {
     func run(_ env: CmdEnv, _ io: CmdIo) -> Bool {
         check(Thread.current.isMainThread)
         let workspace = Workspace.get(byName: args.target.val.raw)
-        let onMonitor = monitors.first { $0.activeWorkspace == workspace }
-        if onMonitor != nil {
-            io.err("Workspace '\(workspace.name)' is already visible on a monitor, returning")
-            return !args.failIfNoop
-        }
 
-        let monitor = focus.workspace.forceAssignedMonitor ?? focus.workspace.workspaceMonitor
+        let curMonitor = focus.workspace.workspaceMonitor
+
+        let monitor = focus.workspace.forceAssignedMonitor ?? curMonitor
         if monitor.activeWorkspace == workspace {
             io.err("Workspace '\(workspace.name)' is already visible on the focused monitor. Tip: use --fail-if-noop to exit with non-zero code")
             return !args.failIfNoop
+        }
+
+        let onMonitor = monitors.first { $0.activeWorkspace == workspace }
+        if let onMonitor = onMonitor {
+            if workspace.forceAssignedMonitor?.monitorId == onMonitor.monitorId {
+                io.err("Workspace '\(workspace.name)' is already visible on a monitor, returning")
+                return !args.failIfNoop
+            }
+            if curMonitor.activeWorkspace.forceAssignedMonitor != nil {
+                io.err("Current Monitor has a pinned workspace, can't swap - returning")
+                return !args.failIfNoop
+            }
+
+            _ = onMonitor.setActiveWorkspace(curMonitor.activeWorkspace)
         }
 
         if isValidAssignment(workspace: workspace, monitor: monitor) {
