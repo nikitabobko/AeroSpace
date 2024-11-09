6634441a2b9222a5e6f68394be67015244730509
Kevin J. Lynagh <kevin@keminglabs.com>
2024-11-09 22:16:53 +0100

    Add `--when-visible (focus|swap)` option to the summon-workspace command.
    What should happen when the summoned workspace is visible on another monitor?
    
    This option introduces a choice to either:
    
    - `focus` the monitor on which the target workspace is already visible (the current behavior)
    - `swap` the workspaces between the monitors, so that the summoned workspace appears on the focused monitor.
    
    The default is `focus`, for backwards compatibility.
    
    Ref: https://github.com/nikitabobko/AeroSpace/issues/603

--- Sources/Common/cmdArgs/impl/SummonWorkspaceCmdArgs.swift
+++ Sources/Common/cmdArgs/impl/SummonWorkspaceCmdArgs.swift
@@ -7,14 +7,33 @@ public struct SummonWorkspaceCmdArgs: CmdArgs {
         help: summon_workspace_help_generated,
         flags: [
             "--fail-if-noop": trueBoolFlag(\.failIfNoop),
+            "--when-visible": SubArgParser(\.rawWhenVisibleAction, upcastSubArgParserFun(parseWhenVisibleAction)),
         ],
         posArgs: [newArgParser(\.target, parseWorkspaceName, mandatoryArgPlaceholder: "<workspace>")],
     )
 
     public var target: Lateinit<WorkspaceName> = .uninitialized
     public var failIfNoop: Bool = false
+    public var rawWhenVisibleAction: WhenVisible? = nil
+
+    public enum WhenVisible: String, CaseIterable, Equatable, Sendable {
+        case focus = "focus"
+        case swap = "swap"
+    }
+}
+
+extension SummonWorkspaceCmdArgs {
+    public var whenVisible: WhenVisible { rawWhenVisibleAction ?? .focus }
 }
 
 private func parseWorkspaceName(i: ArgParserInput) -> ParsedCliArgs<WorkspaceName> {
     .init(WorkspaceName.parse(i.arg), advanceBy: 1)
+}
+
+private func parseWhenVisibleAction(i: SubArgParserInput) -> ParsedCliArgs<SummonWorkspaceCmdArgs.WhenVisible> {
+    if let arg = i.nonFlagArgOrNil() {
+        return .init(parseEnum(arg, SummonWorkspaceCmdArgs.WhenVisible.self), advanceBy: 1)
+    } else {
+        return .fail("<action> is mandatory", advanceBy: 0)
+    }
 }
