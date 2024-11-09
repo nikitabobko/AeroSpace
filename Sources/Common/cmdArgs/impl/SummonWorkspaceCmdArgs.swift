56734b2c7154372f0c913fc352e349d6f76a3513
Kevin J. Lynagh <kevin@keminglabs.com>
2024-11-09 22:16:53 +0100

    Add `--when-visible (focus|swap)` option to the summon-workspace command.
    What should happen when the summoned workspace is visible on another monitor?
    This option introduces a choice to either:
    
    - `focus` the monitor on which the target workspace is already visible (the current behavior)
    - `swap` the workspaces between the monitors, so that the summoned workspace appears on the focused monitor.
    
    The default is `focus`, for backwards compatibility.
    
    Ref: https://github.com/nikitabobko/AeroSpace/issues/603

--- Sources/Common/cmdArgs/impl/SummonWorkspaceCmdArgs.swift
+++ Sources/Common/cmdArgs/impl/SummonWorkspaceCmdArgs.swift
@@ -1,3 +1,5 @@
+private let actio = "<action>"
+
 public struct SummonWorkspaceCmdArgs: CmdArgs {
     public let rawArgs: EquatableNoop<[String]>
     public init(rawArgs: [String]) { self.rawArgs = .init(rawArgs) }
@@ -7,6 +9,7 @@ public struct SummonWorkspaceCmdArgs: CmdArgs {
         help: summon_workspace_help_generated,
         options: [
             "--fail-if-noop": trueBoolFlag(\.failIfNoop),
+            "--when-visible": ArgParser(\.rawWhenVisibleAction, upcastArgParserFun(parseWhenVisibleAction)),
         ],
         arguments: [newArgParser(\.target, parseWorkspaceName, mandatoryArgPlaceholder: "<workspace>")]
     )
@@ -16,8 +19,26 @@ public struct SummonWorkspaceCmdArgs: CmdArgs {
 
     public var target: Lateinit<WorkspaceName> = .uninitialized
     public var failIfNoop: Bool = false
+    public var rawWhenVisibleAction: WhenVisible? = nil
+
+    public enum WhenVisible: String, CaseIterable, Equatable {
+        case focus = "focus"
+        case swap = "swap"
+    }
+}
+
+public extension SummonWorkspaceCmdArgs {
+    var whenVisible: WhenVisible { rawWhenVisibleAction ?? .focus }
 }
 
 private func parseWorkspaceName(arg: String, nextArgs: inout [String]) -> Parsed<WorkspaceName> {
     WorkspaceName.parse(arg)
+}
+
+private func parseWhenVisibleAction(arg: String, nextArgs: inout [String]) -> Parsed<SummonWorkspaceCmdArgs.WhenVisible> {
+    if let arg = nextArgs.nextNonFlagOrNil() {
+        return parseEnum(arg, SummonWorkspaceCmdArgs.WhenVisible.self)
+    } else {
+        return .failure("\(actio) is mandatory")
+    }
 }
