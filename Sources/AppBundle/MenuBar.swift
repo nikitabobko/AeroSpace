63e1c06d1fe39465355b02eef22299ab774b9d50
teavver <jerzyx11@gmail.com>
2025-01-11 10:43:33 +0100

    this one

--- Sources/AppBundle/MenuBar.swift
+++ Sources/AppBundle/MenuBar.swift
@@ -5,7 +5,7 @@ import SwiftUI
 public func menuBar(viewModel: TrayMenuModel) -> some Scene {
     MenuBarExtra {
         let shortIdentification = "\(aeroSpaceAppName) v\(aeroSpaceAppVersion) \(gitShortHash)"
-        let identification      = "\(aeroSpaceAppName) v\(aeroSpaceAppVersion) \(gitHash)"
+        let identification = "\(aeroSpaceAppName) v\(aeroSpaceAppVersion) \(gitHash)"
         Text(shortIdentification)
         Button("Copy to clipboard") { identification.copyToClipboard() }
             .keyboardShortcut("C", modifiers: .command)
@@ -14,10 +14,13 @@ public func menuBar(viewModel: TrayMenuModel) -> some Scene {
             Text("Workspaces:")
             ForEach(viewModel.workspaces, id: \.name) { workspace in
                 Button {
-                    refreshSession(screenIsDefinitelyUnlocked: true) { _ = Workspace.get(byName: workspace.name).focusWorkspace() }
+                    refreshSession(screenIsDefinitelyUnlocked: true) {
+                        _ = Workspace.get(byName: workspace.name).focusWorkspace()
+                    }
                 } label: {
                     Toggle(isOn: .constant(workspace.isFocused)) {
-                        Text(workspace.name + workspace.suffix).font(.system(.body, design: .monospaced))
+                        Text(workspace.name + workspace.suffix).font(
+                            .system(.body, design: .monospaced))
                     }
                 }
             }
@@ -25,20 +28,23 @@ public func menuBar(viewModel: TrayMenuModel) -> some Scene {
         }
         Button(viewModel.isEnabled ? "Disable" : "Enable") {
             refreshSession(screenIsDefinitelyUnlocked: true) {
-                _ = EnableCommand(args: EnableCmdArgs(rawArgs: [], targetState: .toggle)).run(.defaultEnv, .emptyStdin)
+                _ = EnableCommand(args: EnableCmdArgs(rawArgs: [], targetState: .toggle)).run(
+                    .defaultEnv, .emptyStdin)
             }
         }.keyboardShortcut("E", modifiers: .command)
         let editor = getTextEditorToOpenConfig()
         Button("Open config in '\(editor.lastPathComponent)'") {
-            let fallbackConfig: URL = FileManager.default.homeDirectoryForCurrentUser.appending(path: configDotfileName)
+            let fallbackConfig: URL = FileManager.default.homeDirectoryForCurrentUser.appending(
+                path: configDotfileName)
             switch findCustomConfigUrl() {
-                case .file(let url):
-                    url.open(with: editor)
-                case .noCustomConfigExists:
-                    _ = try? FileManager.default.copyItem(atPath: defaultConfigUrl.path, toPath: fallbackConfig.path)
-                    fallbackConfig.open(with: editor)
-                case .ambiguousConfigError:
-                    fallbackConfig.open(with: editor)
+            case .file(let url):
+                url.open(with: editor)
+            case .noCustomConfigExists:
+                _ = try? FileManager.default.copyItem(
+                    atPath: defaultConfigUrl.path, toPath: fallbackConfig.path)
+                fallbackConfig.open(with: editor)
+            case .ambiguousConfigError:
+                fallbackConfig.open(with: editor)
             }
         }.keyboardShortcut("O", modifiers: .command)
         if viewModel.isEnabled {
@@ -51,24 +57,26 @@ public func menuBar(viewModel: TrayMenuModel) -> some Scene {
             terminateApp()
         }.keyboardShortcut("Q", modifiers: .command)
     } label: {
-        viewModel.isEnabled ?
         Text(
-            viewModel.workspaces
-                .filter { !$0.suffix.isEmpty || $0.isFocused }
-                .map { workspace in
-                    let workspaceText = workspace.isFocused 
-                        ? "[ \(workspace.name) ]" 
-                        : workspace.name
-                    return workspaceText
-                }
-                .joined(separator: "  ")
-          )
-        : Text("[ P ]")
-      }
+            viewModel.isEnabled
+                ? viewModel.workspaces
+                    .filter { !$0.suffix.isEmpty || $0.isFocused }
+                    .map { workspace in
+                        let workspaceText =
+                            workspace.isFocused
+                            ? "[ \(workspace.name) ]"
+                            : workspace.name
+                        return workspaceText
+                    }
+                    .joined(separator: "  ")
+                : "[ P ]"
+        )
+    }
 }
 
 func getTextEditorToOpenConfig() -> URL {
-    NSWorkspace.shared.urlForApplication(toOpen: findCustomConfigUrl().urlOrNil ?? defaultConfigUrl)?
-        .takeIf { $0.lastPathComponent != "Xcode.app" } // Blacklist Xcode. It is too heavy to open plain text files
+    NSWorkspace.shared.urlForApplication(
+        toOpen: findCustomConfigUrl().urlOrNil ?? defaultConfigUrl)?
+        .takeIf { $0.lastPathComponent != "Xcode.app" }  // Blacklist Xcode. It is too heavy to open plain text files
         ?? URL(filePath: "/System/Applications/TextEdit.app")
 }
