e61e07d3af6314187af83033ed2211b4d4a88b7e
Nikita Bobko <git@bobko.xyz>
2025-04-26 12:52:52 +0200

    Window detection heuristic hotfix
    _closes https://github.com/nikitabobko/AeroSpace/issues/1324
    
    Until I have a DB that I can test against https://github.com/nikitabobko/AeroSpace/issues/1319
    I will avoid all future changes to "is window" logic

--- Sources/AppBundle/util/AxUiElementEx.swift
+++ Sources/AppBundle/util/AxUiElementEx.swift
@@ -19,7 +19,7 @@ extension AXUIElement {
         }
         // Firefox: Picture in Picture window doesn't have minimize button.
         // todo. bug: when firefox shows non-native fullscreen, minimize button is disabled for all other non-fullscreen windows
-        if app.isFirefox() && get(Ax.minimizeButtonAttr)?.get(Ax.enabledAttr) != true {
+        if id?.isFirefoxId() == true && get(Ax.minimizeButtonAttr)?.get(Ax.enabledAttr) != true {
             return true
         }
         if id == "com.apple.PhotoBooth" { return true }
@@ -67,6 +67,10 @@ extension AXUIElement {
             return false
         }
 
+        if appBundleId?.isFirefoxId() != true {
+            return isWindowHeuristicOld(axApp: axApp, appBundleId: appBundleId)
+        }
+
         // Try to filter out incredibly weird popup like AXWindows without any buttons.
         // E.g.
         // - Sonoma (macOS 14) keyboard layout switch (AXSubrole == AXDialog)
@@ -90,6 +94,42 @@ extension AXUIElement {
             axApp.get(Ax.focusedWindowAttr)?.windowId == self.containingWindowId() ||
 
             get(Ax.subroleAttr) == kAXStandardWindowSubrole
+    }
+
+    private func isWindowHeuristicOld(axApp: AXUIElement, appBundleId: String?) -> Bool { // 0.18.3 hotfix
+        lazy var subrole = get(Ax.subroleAttr)
+        lazy var title = get(Ax.titleAttr) ?? ""
+
+        // Try to filter out incredibly weird popup like AXWindows without any buttons.
+        // E.g.
+        // - Sonoma (macOS 14) keyboard layout switch
+        // - IntelliJ context menu (right mouse click)
+        // - Telegram context menu (right mouse click)
+        if get(Ax.closeButtonAttr) == nil &&
+            get(Ax.fullscreenButtonAttr) == nil &&
+            get(Ax.zoomButtonAttr) == nil &&
+            get(Ax.minimizeButtonAttr) == nil &&
+
+            get(Ax.isFocused) == false &&  // Three different ways to detect if the window is not focused
+            get(Ax.isMainAttr) == false &&
+            axApp.get(Ax.focusedWindowAttr)?.windowId != containingWindowId() &&
+
+            subrole != kAXStandardWindowSubrole &&
+            // Share window purple "pill" indicator has "Window" title https://github.com/nikitabobko/AeroSpace/issues/1101
+            (title.isEmpty || title == "Window") // Maybe it doesn't work in non-English locale
+        {
+            return false
+        }
+        return subrole == kAXStandardWindowSubrole ||
+            subrole == kAXDialogSubrole || // macOS native file picker ("Open..." menu) (kAXDialogSubrole value)
+            subrole == kAXFloatingWindowSubrole || // telegram image viewer
+            appBundleId == "com.apple.finder" && subrole == "Quick Look" // Finder preview (hit space) is a floating window
+    }
+}
+
+private extension String {
+    func isFirefoxId() -> Bool {
+        ["org.mozilla.firefox", "org.mozilla.firefoxdeveloperedition", "org.mozilla.nightly"].contains(self)
     }
 }
 
--- Sources/AppBundle/util/NSRunningApplicationEx.swift
+++ Sources/AppBundle/util/NSRunningApplicationEx.swift
@@ -1,10 +1,6 @@
 import AppKit
 
 extension NSRunningApplication {
-    func isFirefox() -> Bool {
-        ["org.mozilla.firefox", "org.mozilla.firefoxdeveloperedition", "org.mozilla.nightly"].contains(bundleIdentifier ?? "")
-    }
-
     var idForDebug: String {
         "PID: \(processIdentifier) ID: \(bundleIdentifier ?? executableURL?.description ?? "")"
     }
