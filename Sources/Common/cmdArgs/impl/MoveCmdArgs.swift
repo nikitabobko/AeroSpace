4dcc452e777d52dde058432ab2bedcbccb19a183
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2025-05-15 14:55:48 -0300

    feat: add boundaries option to move command

--- Sources/Common/cmdArgs/impl/MoveCmdArgs.swift
+++ Sources/Common/cmdArgs/impl/MoveCmdArgs.swift
@@ -1,3 +1,5 @@
+private let boundar = "<boundary>"
+
 public struct MoveCmdArgs: CmdArgs {
     public let rawArgs: EquatableNoop<[String]>
     fileprivate init(rawArgs: [String]) { self.rawArgs = .init(rawArgs) }
@@ -7,6 +9,7 @@ public struct MoveCmdArgs: CmdArgs {
         help: move_help_generated,
         options: [
             "--window-id": optionalWindowIdFlag(),
+            "--boundaries": ArgParser(\.rawBoundaries, upcastArgParserFun(parseBoundaries)),
         ],
         arguments: [newArgParser(\.direction, parseCardinalDirectionArg, mandatoryArgPlaceholder: CardinalDirection.unionLiteral)]
     )
@@ -14,13 +17,31 @@ public struct MoveCmdArgs: CmdArgs {
     public var direction: Lateinit<CardinalDirection> = .uninitialized
     public var windowId: UInt32?
     public var workspaceName: WorkspaceName?
+    public var rawBoundaries: Boundaries? = nil
 
     public init(rawArgs: [String], _ direction: CardinalDirection) {
         self.rawArgs = .init(rawArgs)
         self.direction = .initialized(direction)
     }
+
+    public enum Boundaries: String, CaseIterable, Equatable, Sendable {
+        case workspace
+        case allMonitorsUnionFrame = "all-monitors-outer-frame"
+    }
+}
+
+public extension MoveCmdArgs {
+    var boundaries: Boundaries { rawBoundaries ?? .workspace }
 }
 
 public func parseMoveCmdArgs(_ args: [String]) -> ParsedCmd<MoveCmdArgs> {
     parseSpecificCmdArgs(MoveCmdArgs(rawArgs: args), args)
+}
+
+private func parseBoundaries(arg: String, nextArgs: inout [String]) -> Parsed<MoveCmdArgs.Boundaries> {
+    if let arg = nextArgs.nextNonFlagOrNil() {
+        return parseEnum(arg, MoveCmdArgs.Boundaries.self)
+    } else {
+        return .failure("\(boundar) is mandatory")
+    }
 }
