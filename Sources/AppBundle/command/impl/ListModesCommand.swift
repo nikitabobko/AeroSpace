3f04a867564129bf6807dc9d4be3f89af6bf88cb
Mikael Carpenter <mikaelcarp.dev@gmail.com>
2025-07-06 21:30:01 -0700

    fix: formatting

--- Sources/AppBundle/command/impl/ListModesCommand.swift
+++ Sources/AppBundle/command/impl/ListModesCommand.swift
@@ -9,14 +9,14 @@ struct ListModesCommand: Command {
             let count = args.current ? 1 : config.modes.count
             return io.out("\(count)")
         }
-        
+
         if args.json {
             let modeNames = args.current ? [activeMode ?? mainModeId] : config.modes.keys.sorted()
             let modeData = modeNames.map { ["mode-id": $0] }
             return JSONEncoder.aeroSpaceDefault.encodeToString(modeData).map(io.out)
                 ?? io.err("Failed to encode JSON")
         }
-        
+
         if args.current {
             return io.out(activeMode ?? mainModeId)
         } else {
