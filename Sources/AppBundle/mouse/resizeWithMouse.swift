11df4774320344c4d2fb2cee6281b340f1d6f2af
Nikita Bobko <git@bobko.xyz>
2025-07-08 22:12:57 +0200

    1/2 Drop screenIsDefinitelyUnlocked
    It won't be necessary after the next commit

--- Sources/AppBundle/mouse/resizeWithMouse.swift
+++ Sources/AppBundle/mouse/resizeWithMouse.swift
@@ -10,7 +10,7 @@ func resizedObs(_ obs: AXObserver, ax: AXUIElement, notif: CFString, data: Unsaf
     Task { @MainActor in
         guard let token: RunSessionGuard = .isServerEnabled else { return }
         guard let windowId, let window = Window.get(byId: windowId), try await isManipulatedWithMouse(window) else {
-            runRefreshSession(.ax(notif), screenIsDefinitelyUnlocked: false)
+            runRefreshSession(.ax(notif))
             return
         }
         resizeWithMouseTask?.cancel()
@@ -30,7 +30,7 @@ func resetManipulatedWithMouseIfPossible() async throws {
         for workspace in Workspace.all {
             workspace.resetResizeWeightBeforeResizeRecursive()
         }
-        runRefreshSession(.resetManipulatedWithMouse, screenIsDefinitelyUnlocked: true, optimisticallyPreLayoutWorkspaces: true)
+        runRefreshSession(.resetManipulatedWithMouse, optimisticallyPreLayoutWorkspaces: true)
     }
 }
 
