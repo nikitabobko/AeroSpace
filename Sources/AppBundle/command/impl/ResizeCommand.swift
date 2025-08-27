928923ce1287834e0b633763e55b63aa85933f3c
Yu Li <YLiLarry@gmail.com>
2025-08-27 00:16:05 -0400

    Resize floating (#1)
    * Support resizing floating windows.
    
    * Make the floating window remains centered during resizing.
    
    ---------
    
    Co-authored-by: Tricster <mediosrity@gmail.com>

--- Sources/AppBundle/command/impl/ResizeCommand.swift
+++ Sources/AppBundle/command/impl/ResizeCommand.swift
@@ -8,6 +8,60 @@ struct ResizeCommand: Command { // todo cover with tests
     func run(_ env: CmdEnv, _ io: CmdIo) -> Bool {
         guard let target = args.resolveTargetOrReportError(env, io) else { return false }
 
+        if let window = target.windowOrNil, window.isFloating {
+            guard let size = window.getSize(), let topLeftCorner = window.getTopLeftCorner() else { return false }
+
+            let computeTopLeftCornerAndSize = { (diffSize: CGSize) -> (CGPoint, CGSize) in
+                let newX = if topLeftCorner.x + size.width + diffSize.width / 2 > target.workspace.workspaceMonitor.width {
+                    max(0, target.workspace.workspaceMonitor.width - size.width - diffSize.width)
+                } else {
+                    max(0, topLeftCorner.x - diffSize.width / 2)
+                }
+
+                let newY = if topLeftCorner.y + size.height + diffSize.height / 2 > target.workspace.workspaceMonitor.height {
+                    max(0, target.workspace.workspaceMonitor.height - size.height - diffSize.height)
+                } else {
+                    topLeftCorner.y - diffSize.height / 2
+                }
+
+                return (CGPoint(x: newX, y: newY), CGSize(width: size.width + diffSize.width, height: size.height + diffSize.height))
+            }
+
+            let isWidthDominant = size.width >= size.height
+            let diff: CGFloat = switch (args.units.val, args.dimension.val) {
+                case (.set(let unit), .width): CGFloat(unit) - size.width
+                case (.set(let unit), .height): CGFloat(unit) - size.height
+                case (.set(let unit), .smart): CGFloat(unit) - (isWidthDominant ? size.width : size.height)
+                case (.set(let unit), .smartOpposite): CGFloat(unit) - (isWidthDominant ? size.height : size.width)
+                case (.add(let unit), _): CGFloat(unit)
+                case (.subtract(let unit), _): -CGFloat(unit)
+            }
+
+            let newTopLeftCorner: CGPoint
+            let newSize: CGSize
+            switch args.dimension.val {
+                case .width:
+                    (newTopLeftCorner, newSize) = computeTopLeftCornerAndSize(CGSize(width: diff, height: 0))
+                case .height:
+                    (newTopLeftCorner, newSize) = computeTopLeftCornerAndSize(CGSize(width: 0, height: diff))
+                case .smart:
+                    let diffSize = if isWidthDominant {
+                        CGSize(width: diff, height: diff * (size.height / size.width))
+                    } else {
+                        CGSize(width: diff * (size.width / size.height), height: diff)
+                    }
+                    (newTopLeftCorner, newSize) = computeTopLeftCornerAndSize(diffSize)
+                case .smartOpposite:
+                    let diffSize = if isWidthDominant {
+                        CGSize(width: diff * (size.width / size.height), height: diff)
+                    } else {
+                        CGSize(width: diff, height: diff * (size.height / size.width))
+                    }
+                    (newTopLeftCorner, newSize) = computeTopLeftCornerAndSize(diffSize)
+            }
+            return window.setFrame(newTopLeftCorner, newSize)
+        }
+
         let candidates = target.windowOrNil?.parentsWithSelf
             .filter { ($0.parent as? TilingContainer)?.layout == .tiles }
             ?? []
