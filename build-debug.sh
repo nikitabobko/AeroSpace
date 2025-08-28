791714fc257f65134b8a970a23f056a24eb10c98
Nikita Bobko <git@bobko.xyz>
2025-08-28 11:18:34 +0200

    Warnings as errors in release builds and tests

--- build-debug.sh
+++ build-debug.sh
@@ -2,10 +2,10 @@
 cd "$(dirname "$0")"
 source ./script/setup.sh
 
-# It takes 300ms for script to complete. It's too long to run in build-debug.sh
+# It takes 300ms for cmd-help to generate. It's too long to run in build-debug.sh, that's why we ignore it
 ./generate.sh --ignore-xcodeproj --ignore-cmd-help
-swift build
-swift build --target AppBundleTests # swift build doesn't build test targets by default :(
+swift build "$@"
+swift build --target AppBundleTests "$@" # swift build doesn't build test targets by default :(
 
 rm -rf .debug && mkdir .debug
 cp -r .build/debug/aerospace .debug
