85fb977905b47a0183f1927486a214244ec27406
Nikita Bobko <git@bobko.xyz>
2025-11-23 23:04:16 +0100

    Performance: avoid AX requests for windows focus when lastNativeFocusedWindowId == windowId
    It greatly helps with Godot back-and-forth style of focus

--- Sources/AppBundle/tree/MacApp.swift
+++ Sources/AppBundle/tree/MacApp.swift
@@ -12,6 +12,7 @@ final class MacApp: AbstractApp {
     private let axApp: ThreadGuardedValue<AXUIElement>
     private let appAxSubscriptions: ThreadGuardedValue<[AxSubscription]> // keep subscriptions in memory
     private let windows: ThreadGuardedValue<[UInt32: AxWindow]> = .init([:])
+    public var lastNativeFocusedWindowId: UInt32? = nil
     private var thread: Thread?
     private var setFrameJobs: [UInt32: RunLoopJob] = [:]
     @MainActor private static var focusJob: RunLoopJob? = nil
@@ -109,11 +110,15 @@ final class MacApp: AbstractApp {
     @MainActor func nativeFocus(_ windowId: UInt32) {
         if serverArgs.isReadOnly { return }
         MacApp.focusJob?.cancel()
-        MacApp.focusJob = withWindowAsync(windowId) { [nsApp] window, job in
-            // Raise firstly to make sure that by the time we activate the app, the window would be already on top
-            window.set(Ax.isMainAttr, true)
-            AXUIElementPerformAction(window, kAXRaiseAction as CFString)
+        if lastNativeFocusedWindowId == windowId {
             nsApp.activate(options: .activateIgnoringOtherApps)
+        } else {
+            MacApp.focusJob = withWindowAsync(windowId) { [nsApp] window, job in
+                // Raise firstly to make sure that by the time we activate the app, the window would be already on top
+                window.set(Ax.isMainAttr, true)
+                AXUIElementPerformAction(window, kAXRaiseAction as CFString)
+                nsApp.activate(options: .activateIgnoringOtherApps)
+            }
         }
     }
 
