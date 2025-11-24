f98e41692b075ec8ac581b1f3c09a796dc2f0f56
Nikita Bobko <git@bobko.xyz>
2025-11-24 22:00:49 +0100

    Package.swift: use '.git' syntax
    Otherwise, I see these warnings in my build log on every rebuild:
    
        warning: 'swift-collections': /Users/bobko/a/AeroSpace/.build/repositories/swift-collections-9a58d5cf is not valid git repository for 'https://github.com/apple/swift-collections', will fetch again.
        Fetching https://github.com/apple/swift-collections from cache
        warning: 'bluesocket': /Users/bobko/a/AeroSpace/.build/repositories/BlueSocket-f6e080fd is not valid git repository for 'https://github.com/Kitura/BlueSocket.git', will fetch again.
        warning: 'hotkey': /Users/bobko/a/AeroSpace/.build/repositories/HotKey-7253600f is not valid git repository for 'https://github.com/soffes/HotKey', will fetch again.
        Fetching https://github.com/Kitura/BlueSocket.git from cache
        Fetching https://github.com/soffes/HotKey from cache
        warning: 'tomlkit': /Users/bobko/a/AeroSpace/.build/repositories/TOMLKit-4b412b01 is not valid git repository for 'https://github.com/LebJe/TOMLKit', will fetch again.
        Fetching https://github.com/LebJe/TOMLKit from cache
        warning: 'antlr4': /Users/bobko/a/AeroSpace/.build/repositories/antlr4-a9dfaf68 is not valid git repository for 'https://github.com/antlr/antlr4', will fetch again.
        warning: 'issoundadditions': /Users/bobko/a/AeroSpace/.build/repositories/ISSoundAdditions-bd7ca9a0 is not valid git repository for 'https://github.com/InerziaSoft/ISSoundAdditions', will fetch again.
        Fetching https://github.com/antlr/antlr4 from cache
    
    Don't ask my why I didn't see them before - I have no clue. Computers...

--- Package.swift
+++ Package.swift
@@ -18,11 +18,11 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "./ShellParserGenerated"),
-        .package(url: "https://github.com/InerziaSoft/ISSoundAdditions", exact: "2.0.1"),
-        .package(url: "https://github.com/Kitura/BlueSocket", exact: "2.0.4"),
-        .package(url: "https://github.com/LebJe/TOMLKit", exact: "0.5.5"),
-        .package(url: "https://github.com/apple/swift-collections", exact: "1.3.0"),
-        .package(url: "https://github.com/soffes/HotKey", exact: "0.2.1"),
+        .package(url: "https://github.com/InerziaSoft/ISSoundAdditions.git", exact: "2.0.1"),
+        .package(url: "https://github.com/Kitura/BlueSocket.git", exact: "2.0.4"),
+        .package(url: "https://github.com/LebJe/TOMLKit.git", exact: "0.5.5"),
+        .package(url: "https://github.com/apple/swift-collections.git", exact: "1.3.0"),
+        .package(url: "https://github.com/soffes/HotKey.git", exact: "0.2.1"),
     ],
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
