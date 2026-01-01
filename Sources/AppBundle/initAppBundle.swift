nikitabobko/AeroSpace

commit ffaee2396c9494ad1c538020b52abf670d0da9d4

Improve error message when users modify default-config.toml
https://github.com/nikitabobko/AeroSpace/discussions/1825

Sources/AppBundle/initAppBundle.swift
@@ -13,7 +13,16 @@ import Foundation
     }
     Task {
         if try await !reloadConfig() {
-            check(try await reloadConfig(forceConfigUrl: defaultConfigUrl))
+            var out = ""
+            check(
+                try await !reloadConfig(forceConfigUrl: defaultConfigUrl, stdout: &out),
+                """
+                Can't load default config. Your installation is probably corrupted.
+                Please don't change default-config.toml
+
+                \(out)
+                """,
+            )
         }
 
         checkAccessibilityPermissions()

commit e767c576b06dcb9902bb34e6dbb3175e0c1cd125

Fix crash when built-in config couldn't be loaded
This horrible boolean regression was introduced in ffaee2396c9494ad1c538020b52abf670d0da9d4

Luckily, it wasn't released yet

Sources/AppBundle/initAppBundle.swift
@@ -15,7 +15,7 @@ import Foundation
         if try await !reloadConfig() {
             var out = ""
             check(
-                try await !reloadConfig(forceConfigUrl: defaultConfigUrl, stdout: &out),
+                try await reloadConfig(forceConfigUrl: defaultConfigUrl, stdout: &out),
                 """
                 Can't load default config. Your installation is probably corrupted.
                 Please don't change default-config.toml