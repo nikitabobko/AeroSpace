13b35332c9d4dab571e3ceec38c6905cd1a0bb39
Nikita Bobko <git@bobko.xyz>
2026-03-09 00:42:56 +0100

    Fix GH actions: don't run periphery on macos 14

--- lint.sh
+++ lint.sh
@@ -4,11 +4,18 @@ source ./script/setup.sh
 
 ./format.sh "$@"
 
-./script/install-dep.sh --periphery
-# Disable superfluous comments detection because it's buggy. todo: report to periphery maintainer
-./.deps/periphery/periphery scan --quiet \
-    --strict \
-    --disable-redundant-public-analysis \
-    --no-superfluous-ignore-comments \
-    --exclude-targets \
-    ShellParserGenerated
+if sw_vers -productVersion | grep -q "^14"; then # macOS 14
+    # dyld[6263]: Library not loaded: /usr/lib/swift/libswiftSynchronization.dylib
+    # Referenced from: <0A48220F-3DCA-3C6C-A54C-FE6C3B854E23> /Users/runner/work/AeroSpace/AeroSpace/.deps/periphery/dist/periphery-3.6.0.artifactbundle/periphery-3.6.0-macos/bin/periphery (built for macOS 15.0 which is newer than running OS)
+    # Reason: tried: '/usr/lib/swift/libswiftSynchronization.dylib' (no such file), '/System/Volumes/Preboot/Cryptexes/OS/usr/lib/swift/libswiftSynchronization.dylib' (no such file), '/usr/lib/swift/libswiftSynchronization.dylib' (no such file, not in dyld cache)
+    echo 'warning: periphery is disabled on macos 14'
+else
+    ./script/install-dep.sh --periphery
+    # Disable superfluous comments detection because it's buggy. todo: report to periphery maintainer
+    ./.deps/periphery/periphery scan --quiet \
+        --strict \
+        --disable-redundant-public-analysis \
+        --no-superfluous-ignore-comments \
+        --exclude-targets \
+        ShellParserGenerated
+fi
