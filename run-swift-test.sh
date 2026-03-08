0174076c8a36483caa6408d163b23a6abd21e4f0
Nikita Bobko <git@bobko.xyz>
2026-03-08 20:30:41 +0100

    run-swift-test: Decrease output verbosity

--- run-swift-test.sh
+++ run-swift-test.sh
@@ -2,7 +2,11 @@
 cd "$(dirname "$0")"
 source ./script/setup.sh
 
-if swift test; then
+if swift test \
+    | sed -E '/^Test (Suite|Case).*(started|passed)/d' \
+    | sed -E '/^[[:space:]]+Executed.*with 0 failures/d' \
+    | sed -E '/ [[:digit:]]+(:[[:digit:]]+)+/s/:/;/g' # Replace colons with semicolons in dates to avoid treating these lines as files in vim
+then
     echo "✅ Swift tests have passed successfully"
 else
     echo "❌ Swift tests have failed"
