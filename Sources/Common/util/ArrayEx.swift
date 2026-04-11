3a4ba7e707dee7189a57997ba75fbed5f018dff6
Nikita Bobko <git@bobko.xyz>
2026-04-11 03:20:27 +0200

    Cleanup: merge AppBundle/util/ArrayEx to Common/util/ArrayEx

--- Sources/Common/util/ArrayEx.swift
+++ Sources/Common/util/ArrayEx.swift
@@ -13,4 +13,30 @@ extension Array {
         }
         return result
     }
+
+    public func singleOrNil(where predicate: (Self.Element) throws -> Bool) rethrows -> Self.Element? {
+        var found: Self.Element? = nil
+        for elem in self where try predicate(elem) {
+            switch found == nil {
+                case true: found = elem
+                case false: return nil
+            }
+        }
+        return found
+    }
+
+    @discardableResult
+    public mutating func remove(element: Self.Element) -> Int? where Self.Element: Equatable {
+        switch firstIndex(of: element) {
+            case nil: return nil
+            case let index?:
+                remove(at: index)
+                return index
+        }
+    }
+}
+
+public func - <T>(lhs: [T], rhs: [T]) -> [T] where T: Hashable {
+    let r = rhs.toSet()
+    return lhs.filter { !r.contains($0) }
 }
