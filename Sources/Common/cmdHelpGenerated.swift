18a455e6f69cdb60a5e7e697bb9dc38906367ab6
Grada <grada23333@outlook.com>
2026-09-01 14:18:03 +0800

    feat: support moving floating windows

--- Sources/Common/cmdHelpGenerated.swift
+++ Sources/Common/cmdHelpGenerated.swift
@@ -131,7 +131,8 @@ let move_workspace_to_monitor_help_generated = """
 let move_help_generated = """
     USAGE: move [-h|--help] [--window-id <window-id>] [--boundaries <boundary>]
                 [--boundaries-action <boundary-action>] [--fail-if-fullscreen]
-                [--fail-if-macos-native-fullscreen] (left|down|up|right)
+                [--fail-if-macos-native-fullscreen] [--floating-pixels <pixels>]
+                (left|down|up|right)
     """
 let reload_config_help_generated = """
     USAGE: reload-config [-h|--help] [--no-gui] [--dry-run] [--warnings-as-errors]
