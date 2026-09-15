82acb1a623ee19840e7323fdbc94930e5e88ea72
Nikita Bobko <git@nbobko.com>
2026-09-15 18:47:17 +0200

    fixup! Add macos-27

--- .github/gh-actions-runner-xcode-select.sh
+++ .github/gh-actions-runner-xcode-select.sh
@@ -8,7 +8,7 @@ sw_vers -productVersion
 # Xcode version affects the target macOS SDK that we compile against + different Xcodes bundle different Swift verions
 if sw_vers -productVersion | grep -q "^14"; then # macOS 14
   sudo xcode-select -s "$XCODE_16_DEVELOPER_DIR"
-elif sw_vers -productVersion | grep -q "^(15|26)"; then # macOS 15 ; macOS 26
+elif sw_vers -productVersion | grep -qE "^(15|26)"; then # macOS 15 ; macOS 26
   sudo xcode-select -s "$XCODE_26_DEVELOPER_DIR"
 else
     :
