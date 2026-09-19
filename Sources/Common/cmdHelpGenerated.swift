824223343f73ee746bc9aa903330deab6f83183a
Dawid van der Hoven <hello@dawid.codes>
2026-09-19 14:56:10 +0200

    Add split-* dimensions to resize command
    'resize width|height|smart' grows or shrinks the focused window. Which
    way the border moves depends on where the window is: 'resize width +50'
    moves the border right for the left window and left for the right
    window. It isn't possible to bind a key to "move this border left".
    It gets worse in the nested layouts that auto tiling produces, where
    the user thinks in terms of borders between tiles rather than sizes of
    windows. Also, width/height take the space from all the siblings, which
    moves borders that the user didn't mean to touch.
    
    Add split-left, split-right, split-up and split-down dimensions that
    move the split (the border between the window and its neighbour) in
    the given direction:
    
        resize split-right +50
    
    The split is the trailing (right/bottom) border of the closest node,
    starting from the window and going up the tree, whose parent has the
    matching orientation and more than one child. If that node is the last
    one in its container, the split is its leading (left/top) border. Only
    the two neighbours that share the split are resized: one gets the
    delta, the other one loses it.
    
    The number must be prefixed with '+' or '-' ('-' moves the split in
    the opposite direction), since an absolute value has no meaning for a
    direction. The command fails if there is no split in that direction,
    or if one of the neighbours would get a non-positive weight.
    
    The docs, the generated help and the commands grammar are updated.

--- Sources/Common/cmdHelpGenerated.swift
+++ Sources/Common/cmdHelpGenerated.swift
@@ -138,6 +138,7 @@ let reload_config_help_generated = """
     """
 let resize_help_generated = """
     USAGE: resize [-h|--help] [--window-id <window-id>] (smart|smart-opposite|width|height) [+|-]<number>
+       OR: resize [-h|--help] [--window-id <window-id>] (split-left|split-right|split-up|split-down) (+|-)<number>
     """
 let run_callback_help_generated = """
     USAGE: run-callback [-h|--help] [--for-every-window|--window-id <window-id>] on-window-detected
